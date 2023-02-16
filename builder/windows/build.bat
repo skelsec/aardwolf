f80b14a139f47ba74ace1e878baf71b5bea7dc41
SkelSec <skelsec@skelsecprojects.com>
2023-02-16 08:46:17 -0800

    adding builder

--- builder/windows/build.bat
+++ builder/windows/build.bat
@@ -1,3 +1,4 @@
-
-cd ..\..\
-pip wheel . -w dist --no-deps
+C:\Python38\python.exe -m pip wheel ..\..\ -w ..\..\dist --no-deps
+C:\Python39\python.exe -m pip wheel ..\..\ -w ..\..\dist --no-deps
+C:\Python310\python.exe -m pip wheel ..\..\ -w ..\..\dist --no-deps
+C:\Python311\python.exe -m pip wheel ..\..\ -w ..\..\dist --no-deps
