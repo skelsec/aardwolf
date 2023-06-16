03b15d9f1fe392072e0a80e6a42d5f500ac4c8bc
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2023-06-16 18:31:58 +0000

    Add support for copying directories

--- aardwolf/extensions/RDPECLIP/clipboard.py
+++ aardwolf/extensions/RDPECLIP/clipboard.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass
-from pathlib import PurePath
+from pathlib import Path, PurePath
 from typing import Dict, Iterable, Optional, List, Protocol
 
 from aardwolf.commons.queuedata.clipboard import RDP_CLIPBOARD_DATA, RDP_CLIPBOARD_DATA_FILELIST, RDP_CLIPBOARD_DATA_TXT
@@ -12,12 +11,6 @@ class ClipboardHandler(Protocol):
 		pass
 
 
-@dataclass
-class FileData:
-	path:PurePath # The full path to the file
-	file_descriptor:CLIPRDR_FILEDESCRIPTOR # The file descriptor
-
-
 class Clipboard:
 	def __init__(self, file_provider:FileProvider = None):
 		self._file_provider = FilesystemFileProvider() if file_provider is None else file_provider
@@ -26,7 +19,7 @@ class Clipboard:
 		self._next_format_id = 0xC000
 		self._handlers:List[ClipboardHandler] = []
 		self.file_copy_id = self.register_format('FileGroupDescriptorW')
-		self._file_data: List[FileData] = []
+		self._file_paths: List[PurePath] = []
 
 	@property
 	def formats(self) -> Dict[int, str]:
@@ -45,22 +38,22 @@ class Clipboard:
 		self._handlers.append(handler)
 
 	def get_file_size(self, index:int) -> int:
-		file_data = self._get_file_at_index(index)
-		if file_data is None:
+		file_path = self._get_file_at_index(index)
+		if file_path is None:
 			return 0
 
-		return self._file_provider.get_file_size(str(file_data.path))
+		return self._file_provider.get_file_size(str(file_path))
 
 	def get_file_data(self, index:int, start:int, count:int) -> bytes:
-		file_data = self._get_file_at_index(index)
-		if file_data is None:
+		file_path = self._get_file_at_index(index)
+		if file_path is None:
 			return b''
 
-		return self._file_provider.get_file_data(str(file_data.path), start, count)
+		return self._file_provider.get_file_data(str(file_path), start, count)
 
-	def _get_file_at_index(self, index:int) -> Optional[FileData]:
-		if index < len(self._file_data):
-			return self._file_data[index]
+	def _get_file_at_index(self, index:int) -> Optional[PurePath]:
+		if index < len(self._file_paths):
+			return self._file_paths[index]
 			
 		return None
 
@@ -72,15 +65,33 @@ class Clipboard:
 	async def set_current_clipboard_files(self, files:Iterable[PurePath]):
 		file_list = CLIPRDR_FILELIST()
 		for f in files:
-			file = CLIPRDR_FILEDESCRIPTOR()
-			file.flags = FD_FLAGS.ATTRIBUTES
-			file.fileAttributes = FILE_ATTRIBUTE.NORMAL # Note: not handling directories
-			file.fileName = f.parts[-1]
-			self._file_data.append(FileData(path=f, file_descriptor=file))
-			file_list.fileDescriptorArray.append(file)
+			self._file_paths.append(f)
+
+			path = Path(f)
+			file_descriptor = self._create_filedescriptor(path)
+			file_list.fileDescriptorArray.append(file_descriptor)
+			self._recurse_path(path, file_list.fileDescriptorArray)
 
 		data = RDP_CLIPBOARD_DATA_FILELIST(data=file_list, datatype=self.file_copy_id)
 		await self.set_data(data)
+
+	def _create_filedescriptor(self, path:Path, depth:int = 0) -> CLIPRDR_FILEDESCRIPTOR:
+		file = CLIPRDR_FILEDESCRIPTOR()
+		file.flags = FD_FLAGS.ATTRIBUTES
+		attributes = FILE_ATTRIBUTE.DIRECTORY if path.is_dir() else FILE_ATTRIBUTE.NORMAL
+		file.fileAttributes = attributes
+		file.fileName = str(Path(*path.parts[-(1 + depth):]))
+		return file
+
+	def _recurse_path(self, path:Path, file_descriptors:List[CLIPRDR_FILEDESCRIPTOR], depth:int = 0):
+		if not path.is_dir():
+			return
+		
+		for p in path.iterdir():
+			self._file_paths.append(p)
+			file_descriptor = self._create_filedescriptor(p, depth + 1)
+			file_descriptors.append(file_descriptor)
+			self._recurse_path(p, file_descriptors, depth + 1)
 
 	async def get_current_clipboard_text(self) -> str:
 		if self.data is None or self.data.datatype not in [CLIPBRD_FORMAT.CF_UNICODETEXT]:
@@ -96,7 +107,7 @@ class Clipboard:
 			return
 
 		if data.datatype != self.file_copy_id:
-			self._file_data = []
+			self._file_paths = []
 
 		self.data = data
 		await self.notify_copy(data)
